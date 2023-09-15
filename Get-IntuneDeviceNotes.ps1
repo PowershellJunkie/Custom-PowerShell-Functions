776ea84008a94deae3f1dac2a5bb8382f6bb26d1
PowershellJunkie <86009671+PowershellJunkie@users.noreply.github.com>
2023-09-15 11:17:24 -0400

    Create Get-IntuneDeviceNotes.ps1

---
+++ Get-IntuneDeviceNotes.ps1
@@ -0,0 +1,44 @@
+Function Get-IntuneDeviceNotes{
+    <#
+    .SYNOPSIS
+    Gets the notes of a device in intune.
+    
+    .DESCRIPTION
+    Gets the notes property on a device in intune using the beta Graph api
+    
+    .PARAMETER DeviceName
+    The name of the device that you want to get the notes field from as it appears in intune.
+    
+    .EXAMPLE
+    Get-IntuneDeviceNotes -DeviceName TestDevice01
+    
+    .NOTES
+    Must connect to the graph api first with Connect-MSGraph.
+
+    Not created by me and no creator name to credit. Minor edits to make this work correctly.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $DeviceName
+    )
+    Try {
+        $DeviceID = (Get-IntuneManagedDevice -filter "deviceName eq '$DeviceName'" -ErrorAction Stop).id
+    }
+    Catch {
+        Write-Error $_.Exception.Message
+        break
+    }
+    $deviceId = (Get-IntuneManagedDevice -Filter "deviceName eq '$DeviceName'").id
+    $Resource = "deviceManagement/managedDevices('$deviceId')"
+    $properties = 'notes'
+    $uri = "https://graph.microsoft.com/beta/$($Resource)?select=$properties"
+    Try{
+        (Invoke-MSGraphRequest -HttpMethod GET -Url $uri -ErrorAction Stop).notes
+    }
+    Catch{
+        Write-Error $_.Exception.Message
+        break
+    }
+}
